8001901185f297bac8049083b6903cc05e065894
Endris Mohammed <endrismd@gmail.com>
2022-07-05 16:52:26 -0700

    Prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "main.h"                                                                                                  
-                                                                                                             
-/**                                                                                                                
- * main- prints _putchar character in a new line                                                                   
- *                                                                                                                 
- * Return: 0                                                                                                       
- */                                                                                                                
-int main(void)                                                                                                    
-{                                                                                                                  
-	_putchar('_');                                                                                             
-        _putchar('p');                                                                                             
-	_putchar('u');                                                                                             
-        _putchar('t');                                                                                             
-	_putchar('c');                                                                                             
-	_putchar('h');                                                                                             
-	_putchar('a');                                                                                             
-        _putchar('r');                                                                                             
-        _putchar('\n');                                                                                            
+#include "main.h"
+
+/**
+ * main- prints _putchar character in a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
