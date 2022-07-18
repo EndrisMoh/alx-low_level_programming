eb311b393c1070b51b1bef8946b67764f9f9f725
Endris Mohammed <endrismd@gmail.com>
2022-07-18 13:44:35 -0700

    A function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+void *memcpy(void *dest, const void *src, size_t n);
+/**
+ * main - function that copies memory area
+ *@dest: pointer to char tyep
+ *@src: integer
+ *@n: unsigned int n
+ *Return: @s char value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	void *p =  dest;
+
+	memcpy(p, (char *)src, n);
+	return (dest);
+}
