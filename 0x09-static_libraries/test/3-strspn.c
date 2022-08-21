7c8c58ccab49f5b76710d2097fb6ffd2e943ec2f
Endris Mohammed <endrismd@gmail.com>
2022-08-20 23:56:40 -0700

    All files for testing

---
+++ 0x09-static_libraries/test/3-strspn.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - function that get the length of a prefix substring
+ *@s: pointer of char type
+ *@accept: pointer of character type
+ *Return: Returns the number of bytes in the initial segment of s
+ *which consist only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int numbyt = strspn(s, accept);
+
+	return (numbyt);
+}
